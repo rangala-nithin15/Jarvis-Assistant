1a4616cdda4c228a97ef78fe6e5d4b6604b1bbc3
rangala nithin <158316590+rangala-nithin15@users.noreply.github.com>
2026-02-17 22:17:16 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,31 @@
+let waveInterval;
+
+function activate() {
+    const consoleBox = document.getElementById("console");
+    consoleBox.innerHTML += "<br>> Initializing JARVIS...";
+
+    startWave();
+
+    fetch('/activate', { method: 'POST' })
+    .then(response => response.json())
+    .then(data => {
+        consoleBox.innerHTML += "<br>> " + data.status;
+        consoleBox.innerHTML += "<br>> Voice Systems Online";
+    });
+}
+
+function startWave() {
+    const bars = document.querySelectorAll(".waveform span");
+
+    waveInterval = setInterval(() => {
+        bars.forEach(bar => {
+            const randomHeight = Math.floor(Math.random() * 40) + 10;
+            bar.style.height = randomHeight + "px";
+        });
+    }, 200);
+}
+
+function stopWave() {
+    clearInterval(waveInterval);
+}
 
